2b74f68382bf16dd883bad285ef2e1799889b20e
feruxmax <feruxmax@gmail.com>
2016-10-02 00:52:06 +0300

    add scalling

--- test_logistic_regression.py
+++ test_logistic_regression.py
@@ -7,13 +7,14 @@ Created on Wed Sep 21 19:11:53 2016
 import pandas as panda
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import KFold
-from sklearn.cross_validation import *
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import cross_val_score
 
 def score_logistic(X, y, C):
     clf = LogisticRegression(penalty='l2', C=C)
     kf = KFold(y.size, n_folds=5, shuffle=True, random_state=1)
     score = cross_val_score(clf, X, y, scoring='roc_auc', cv=kf)    
-    return score.mean()
+    return score
 
 
 dataset = panda.read_csv("evidance_event_android.csv")
@@ -22,10 +23,10 @@ trg = dataset['process_orderSendSuccess_event'].replace(2, 1)
 trn = dataset.drop(['process_orderSendSuccess_event','android_id','event_datetime'], axis=1)
 #toto = trg.index()
 #togo = trn.index()
-rezult = score_logistic(trn, trg, 0.5)
-print(rezult)
-'''    
+
 scaler = StandardScaler()
-scaler.fit(X)
-X_orig_scale = scaler.transform(X)
-'''
+scaler.fit(trn)
+X = scaler.transform(trn)
+
+rezult = score_logistic(X, trg, 0.5)
+print(rezult)
