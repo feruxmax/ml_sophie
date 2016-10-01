a66a7dca87ce98d5e4834f5527f64f8d0b4de700
777Max777 <maximus-lihotin@mail.ru>
2016-10-01 22:06:27 +0400

    Использовал ту функцию, которую вы мне скидывали... Проблема в том, что зацикливается в cross_val_score и у меня процессор нагружается до 95% и греется до 91 градуса :-) В общем я сам не понял что натворил здесь

---
+++ test_logistic_regression.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 19:11:53 2016
+    
+@author: Лихотин
+`"""
+import pandas as panda
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import KFold
+from sklearn.cross_validation import *
+
+def score_logistic(X, y, C):
+    clf = LogisticRegression(penalty='l2', C=C)
+    kf = KFold(y.size, n_folds=5, shuffle=True, random_state=1)
+    score = cross_val_score(clf, X, y, scoring='roc_auc', cv=kf, n_jobs=-1)    
+    return score.mean()
+
+
+dataset = panda.read_csv("evidance_event_android.csv")
+
+trg = dataset['process_orderSendSuccess_event']
+trn = dataset.drop(['process_orderSendSuccess_event','android_id','event_datetime'], axis=1)
+#toto = trg.index()
+#togo = trn.index()
+rezult = score_logistic(trn, trg, 0.005)
+print(rezult)
+'''    
+scaler = StandardScaler()
+scaler.fit(X)
+X_orig_scale = scaler.transform(X)
+'''
