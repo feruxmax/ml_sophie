bc2dfb7f59a74571b99ffcc07de19910f89e6ad6
feruxmax <feruxmax@gmail.com>
2016-11-04 22:10:18 +0300

    add names to srore

--- learning.py
+++ learning.py
@@ -8,10 +8,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 
-
 def prepareXy(data):
     y = data['process_orderSendSuccess_event'].replace(2, 1).replace(3, 1)
-    X = data.drop(['process_orderSendSuccess_event','ios_ifv','event_datetime'],
+    X = data.drop(['process_orderSendSuccess_event','id','event_datetime'],
                  axis=1)
         
     return (X, y)
@@ -29,14 +28,14 @@ def scale(X):
 models = {"boosting": GradientBoostingClassifier(n_estimators=500, random_state=241),
           "logistic": LogisticRegression(penalty='l2', C=0.5)}
 
-dataset = pd.read_csv("evidance_event_ios.csv")
+dataset = pd.read_csv("data/ios_T=2h_d=1T.csv")
 X, y = prepareXy(dataset)
-X = scale(X)           
-print(crossval(X, y, models['boosting'], 'precision').mean())
-print(crossval(X, y, models['boosting'], 'recall').mean())
+#X = scale(X)           
+print("presision: ", crossval(X, y, models['boosting'], 'precision').mean())
+print("recall: ", crossval(X, y, models['boosting'], 'recall').mean())
 
 #%% 
 X_learn, X_test, y_learn, y_test = train_test_split(X, y, test_size=0.4)
 models["boosting"].fit(X_learn, y_learn)
 forest_pred = models["boosting"].predict(X_test)
-print(accuracy_score(forest_pred, y_test))
+print("accuracy: ", accuracy_score(forest_pred, y_test))
