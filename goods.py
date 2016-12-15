2df0e25a2493ecd61f748aef919c9491b693c58a
feruxmax <feruxmax@gmail.com>
2016-12-15 14:13:52 +0300

    fix feature extraction, use AgglomerativeClustering

--- goods.py
+++ goods.py
@@ -5,31 +5,45 @@ import numpy as np
 from sklearn import manifold
 from sklearn.cluster import KMeans
 from sklearn.cluster import MeanShift 
+from sklearn.cluster import AgglomerativeClustering
+from time import time
 #%%
 data = pd.read_csv('data/ml_requests.csv')
 
 data.head()
+#%%
 items = data['items'].str.split(',')
 features_names = set().union(*items)
 
 ##%%
-features = pd.DataFrame(index=items.index, columns=features_names)
+orders = pd.DataFrame(index=items.index, columns=features_names)
 
 for i in items.index:
-    features.iloc[i] = dict.fromkeys(items.iloc[i], 1)
+    orders.iloc[i] = dict.fromkeys(items.iloc[i], 1)
 
-features.fillna(0, inplace=True)
-features = features.reindex_axis(sorted(features.columns), axis=1)
+orders.fillna(0, inplace=True)
+orders = orders.reindex_axis(sorted(orders.columns), axis=1)
+orders.head()
+
+#%%
+user_ids = data['id_creator'].unique()
+features = pd.DataFrame(index=user_ids, columns=orders.columns)
+
+for i in features.index:
+    features.loc[i] = orders[data['id_creator']==i].sum()
+
 features.head()
 
 #%%
 X = features
 n_components=2
 
-colors = KMeans(n_clusters=2).fit_predict(X)
+#colors = KMeans(n_clusters=2).fit_predict(X)
+#colors = MeanShift().fit_predict(X)
+colors = AgglomerativeClustering(n_clusters=2).fit_predict(X)
 colors
 
-#%%
+##%
 t0 = time()
 mds = manifold.MDS(n_components, max_iter=100, n_init=1)
 Y = mds.fit_transform(X)
